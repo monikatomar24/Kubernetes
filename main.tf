19262dc7d60cc90b6d1cca8bb15466cf48ed14b7
monikatomar24 <84930509+monikatomar24@users.noreply.github.com>
2022-05-09 08:24:59 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,78 @@
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 =  var.gcp_project_id
+  name                       =  var.gke_cluster_name
+  region                     =  var.gcp_region
+  ip_range_pods              = "default"
+  ip_range_services          = "default"
+  zones                      =  var.gke_zones
+  network                    =  var.gke_network
+  subnetwork                 =  var.gke_subnetwork
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+
+  node_pools = [
+    {
+      name                      = var.gke_default_nodepool_name
+      machine_type              = "e2-medium"
+      min_count                 = 1
+      max_count                 = 3
+      local_ssd_count           = 0
+      disk_size_gb              = 100
+      disk_type                 = "pd-standard"
+      image_type                = "COS_CONTAINERD"
+      auto_repair               = true
+      auto_upgrade              = true
+      service_account           = var.gke_service_account_name
+      preemptible               = true
+      initial_node_count        = 1
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = []
+
+    default-node-pool = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
+    }
+  }
+
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
+  }
+}
